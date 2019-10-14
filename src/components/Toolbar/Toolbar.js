d130d4aee56bbb3bab3a5454db45cb006ed4a3c9
Camilla Damasceno <camilla.damasceno@dtidigital.com.br>
2019-10-14 15:53:15 -0300

    criação pastas para mobile

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import './Toolbar.css';
+import '../SideDrawer/DrawerToggleButton';
 
 const toolbar = props => (
     <header className="toolbar">
         <nav className="toolbar_navigation">
+            <div></div>
             <div className="navigation">
                 <div className="toolbar_logo"><a href="/">THE LOGO</a></div>
                 <div className="toolbar_navigation_items">
