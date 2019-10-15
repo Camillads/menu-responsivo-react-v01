7d917cc8794b3af4f8aff5b0e35082c79eacbf33
Camilla Damasceno <camilla.damasceno@dtidigital.com.br>
2019-10-15 10:38:03 -0300

    criação do drawer e backdrop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Toolbar from './components/Toolbar/Toolbar';
+import SideDrawer from './components/SideDrawer/SideDrawer';
 
 function App() {
   return (
     <div className="App">
       <Toolbar />
-      <main style={{marginTop: '5%'}}>
+      <SideDrawer />
+      <main style={{ height: '100%' }}>
         <div>This is page content!</div>
       </main>
     </div>
